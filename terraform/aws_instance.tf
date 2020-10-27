e494934650bb00e126c8f1d972559c9e57a7a59c
jiseooooon <57252187+jiseooooon@users.noreply.github.com>
2020-10-27 13:24:41 +0900

    Update aws_instance.tf

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -2,9 +2,7 @@ resource "aws_instance" "terraform-test" {
   ami = "ami-0a2778941dc6f2820"
   instance_type = "t2.micro"
   key_name = "BESPIN_TEST_YHM"
-  vpc_security_groups = [
-    "sg-02db55411a5e10dff"
-  ]
+  vpc_security_group_ids = ["sg-02db55411a5e10dff"]
   subnet_id="subnet-1af11756"
   tags = {
     Name = "test-qa-test"
